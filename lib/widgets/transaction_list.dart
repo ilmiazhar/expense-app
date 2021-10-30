609b84057785cb3456e59fde52df3fce4dcbe4fc
ilmiazhar <iing25leo60@gmail.com>
2021-10-30 23:01:27 +0700

    Implement different method to make transaction list scrollable with replacing main Column widget in transaction_list.dart file with ListView widget.

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -12,52 +12,49 @@ class TransactionList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 250,
-      child: SingleChildScrollView(
-        child: Column(
-          children: transactions.map((tx) {
-            return Card(
-              child: Row(
-                children: <Widget>[
-                  Container(
-                      margin:
-                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                      decoration: BoxDecoration(
-                        border: Border.all(
-                          color: generateMaterialColor(Pallete.fourth),
-                          width: 2,
-                        ),
+      child: ListView(
+        children: transactions.map((tx) {
+          return Card(
+            child: Row(
+              children: <Widget>[
+                Container(
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: generateMaterialColor(Pallete.fourth),
+                        width: 2,
                       ),
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        "\$${tx.amount}",
-                        style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 20,
-                            color: generateMaterialColor(Pallete.fourth)),
-                      )),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        tx.title,
-                        style: TextStyle(
+                    ),
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      "\$${tx.amount}",
+                      style: TextStyle(
                           fontWeight: FontWeight.bold,
-                          fontSize: 18,
-                          color: generateMaterialColor(Pallete.fourth),
-                        ),
+                          fontSize: 20,
+                          color: generateMaterialColor(Pallete.fourth)),
+                    )),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      tx.title,
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                        color: generateMaterialColor(Pallete.fourth),
                       ),
-                      Text(
-                        DateFormat("d MMM y").format(tx.date),
-                        style: TextStyle(
-                            color: generateMaterialColor(Pallete.primary)),
-                      )
-                    ],
-                  )
-                ],
-              ),
-            );
-          }).toList(),
-        ),
+                    ),
+                    Text(
+                      DateFormat("d MMM y").format(tx.date),
+                      style: TextStyle(
+                          color: generateMaterialColor(Pallete.primary)),
+                    )
+                  ],
+                )
+              ],
+            ),
+          );
+        }).toList(),
       ),
     );
   }
